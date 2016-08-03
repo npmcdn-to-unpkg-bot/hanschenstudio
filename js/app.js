c378b3df57cb48f60077cac2d92e0f7e2d0f9e44
hanschenstudio <hanschenstudio@gmail.com>
2016-08-02 21:14:15 -0400

    Add nav bar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,64 @@
-$(document).ready(function() {
-	$(".fancybox").fancybox({
-		openEffect	: 'none',
-		closeEffect	: 'none'
+
+
+angular.module("HCCStudioApp", ["ngRoute", "ngAnimate"])
+	.config(["$routeProvider", function($routeProvider) {
+		$routeProvider.when("/", {
+			templateUrl: "templates/gallery.html",
+			controller: "GalleryController",
+			controllerAs: "vm"
+		})
+		.when("/gallery", {
+			templateUrl: "templates/gallery.html",
+			controller: "GalleryController",
+			controllerAs: "vm"
+		})
+		.when("/about", {
+			templateUrl: "templates/about.html",
+			controller: "AboutController",
+			controllerAs: "vm"
+		})
+		.when("/contact", {
+			templateUrl: "templates/contact.html",
+			controller: "ContactController",
+			controllerAs: "vm"
+		})
+		.when("/error", {
+			templateUrl: "templates/error.html",
+			controller: "ErrorController",
+			controllerAs: "vm"
+		})
+		.otherwise({
+			redirectTo: "/error"
+		});
+	}])
+
+	.controller("GalleryController", function() {
+		$(document).ready(function() {
+			$(".fancybox").fancybox({
+				openEffect	: 'none',
+				closeEffect	: 'none'
+			});
+		
+		 $(function(){
+ 
+    var $container = $('#container');
+  
+    $container.imagesLoaded( function(){
+      $container.masonry({
+        itemSelector : '.masonryImage'
+      });
+    });
+  
+  });
+		});
+	})
+	.controller("AboutController", function() {
+
+	})
+	.controller("ContactController", function() {
+
+	})
+	.controller("ErrorController", function() {
+		
 	});
-});
+
