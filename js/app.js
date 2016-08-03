329cd370d75dfa68bf23c5a81e54bc1e312001a5
hanschenstudio <hanschenstudio@gmail.com>
2016-08-02 21:39:34 -0400

    New php submit form

--- js/app.js
+++ js/app.js
@@ -56,7 +56,15 @@ angular.module("HCCStudioApp", ["ngRoute", "ngAnimate"])
 
 	})
 	.controller("ContactController", function() {
-
+		function SubmitForm() {
+			var name = $("#name").val();
+			var email = $("#email").val();
+			var phone = $("#phone").val();
+			$.post("submit.php", { name: name, email: email, phone: phone },
+			   function(data) {
+			     alert("Data Loaded: " + data);
+			   });
+			}
 	})
 	.controller("ErrorController", function() {
 		
